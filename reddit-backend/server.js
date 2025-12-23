eb65feccb5e72e222cdca6a20a8de07e4e4b4a09
AhmedZahrann <ahmed.zahran0005@gmail.com>
2025-12-23 13:38:31 +0200

    Fix CORS for Vercel frontend

--- reddit-backend/server.js
+++ reddit-backend/server.js
@@ -16,11 +16,17 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://reddit-clone-gamma-livid.vercel.app"
+    ],
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
